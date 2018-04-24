b32b022af22e958cf418dca6cc79512da2b049ac
stephensenjaya <senjaya.stephen@gmail.com>
2018-04-24 13:44:24 +0700

    update customer sudah benar sepertinya sialakan di cek

--- StoredProcedure/updateCostumer.sql
+++ StoredProcedure/updateCostumer.sql
@@ -1,52 +1,67 @@
+DROP PROCEDURE IF EXISTS 'updateHubungan';
+DROP TEMPORARY Table IF EXISTS customerSementara;
+DELIMITER $$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateCustomer`(
 in idCustomer int,
 in nama varchar (100),
 in idLokasi INT,
 in tanggalLahir date,
 in alamat varchar(100),
-in nilaiInvestasi int
+in nilaiInvestasi int(11)
 )
 BEGIN
-	DECLARE tempNama int;
+
+
+	DECLARE tempNama varchar(100);
 	DECLARE tempidLokasi int;
-	DECLARE tempTanggalLahir int;
-	DECLARE tempAlamat int;
-	DECLARE tempNilaiInvestasi int;
-	CREATE TEMPORARY TABLE customerSementara (nama varchar(100), idLokasi int, tanggalLahir date, alamat varchar(100), nilaiInvestasi int);
+	DECLARE tempTanggalLahir date;
+	DECLARE tempAlamat varchar(100);
+	DECLARE tempNilaiInvestasi int(11);
+    
+	CREATE TEMPORARY TABLE customerSementara (
+		nama varchar(100), 
+        idLokasi int,
+        tanggalLahir date,
+        alamat varchar(100),
+        nilaiInvestasi int);
     INSERT INTO customerSementara
 	SELECT
-		nama,
-        idLokasi,
-        tanggalLahir,
-        alamat,
-        nilaiInvestasi
+		Customer.nama,
+        Customer.idLokasi,
+        Customer.tanggalLahir,
+        Customer.alamat,
+        Customer.nilaiInvestasi
 	FROM 
 		Customer
 	WHERE
 		Customer.idCustomer = idCustomer;
         
-	Select nama into tempNama FROM customerSementara;
-    Select idLokasi into tempIdLokasi FROM customerSementara;
-    Select tanggalLahir into tempTanggalLahir FROM customerSementara;
-    Select alamat into tempAlamat FROM customerSementara;
-    Select nilaiInvestasi into tempNilaiInvestasi FROM customerSementara;
-	
+	Select customerSementara.nama into tempNama FROM customerSementara;
+    Select customerSementara.idLokasi into tempIdLokasi FROM customerSementara;
+    Select customerSementara.tanggalLahir into tempTanggalLahir FROM customerSementara;
+    Select customerSementara.alamat into tempAlamat FROM customerSementara;
+    Select customerSementara.nilaiInvestasi into tempNilaiInvestasi FROM customerSementara;
+		
     IF tempNama != nama THEN 
 		call insertPerubahanCustomer(idcustomer,'Customer','nama',tempnama,NULL,1);
 		UPDATE customer SET customer.nama = nama WHERE customer.idCustomer = idCustomer;
-    ELSEIF tempIdLokasi != idLokasi THEN 
+    END IF;
+    IF tempIdLokasi != idLokasi THEN 
 		call insertPerubahanCustomer(idcustomer,'Customer','idLokasi',tempidLokasi,NULL,1);
         UPDATE customer SET customer.idLokasi = idLokasi WHERE customer.idCustomer = idCustomer;
-    ELSEIF tempTanggalLahir != tanggalLahir THEN 
+    END IF;
+    IF tempTanggalLahir != tanggalLahir THEN 
 		call insertPerubahanCustomer(idcustomer,'Customer','tanggalLahir',temptanggalLahir,NULL,1);
         UPDATE customer SET customer.tanggalLahir = tanggalLahir WHERE customer.idCustomer = idCustomer;
-    ELSEIF tempAlamat != alamat THEN 
+    END IF;
+    IF tempAlamat != alamat THEN 
 		call insertPerubahanCustomer(idcustomer,'Customer','alamat',tempalamat,NULL,1);
         UPDATE customer SET customer.alamat = alamat WHERE customer.idCustomer = idCustomer;
-    ELSEIF tempNilaiInvestasi != nilaiInvestasi THEN 
-		call insertPerubahanCustomer(idcustomer,'Customer','nilaiInvestasi',tempnilaiInvestasi,NULL,1);
-        UPDATE customer SET customer.nilaiInvestasi = nama WHERE customer.nilaiInvestasi = idCustomer;
+    END IF;
+    IF tempNilaiInvestasi != nilaiInvestasi THEN 
+		call insertPerubahanCustomer(idcustomer,'Customer','nilaiInvestasi',tempNilaiInvestasi,NULL,1);
+        UPDATE customer SET customer.nilaiInvestasi = nilaiInvestasi WHERE customer.idCustomer = idCustomer;
 	END IF;
-    
-	DROP TABLE customerSementara;
+    DROP TEMPORARY Table customerSementara;
 END
