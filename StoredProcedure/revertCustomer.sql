902c175299ab9da6f0164041d02cebb866573643
stephensenjaya <senjaya.stephen@gmail.com>
2018-05-12 23:19:51 +0700

    fix revert customer

--- StoredProcedure/revertCustomer.sql
+++ StoredProcedure/revertCustomer.sql
@@ -1,13 +1,13 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `revertCustomer`(
-id int
+id int,
+idAdmin int
 )
 BEGIN
-DECLARE waktu datetime;
 DECLARE v_finished INTEGER DEFAULT 0;
 DECLARE namaField1 varchar(1000);
 declare valueSebelum1 varchar(1000);
-DECLARE que varchar(1000);
-
+DECLARE waktu datetime;
+DECLARE temp varchar(1000);
 declare myCursor cursor for
     SELECT namaField,valueSebelum
     FROM perubahanCustomer 
@@ -19,34 +19,43 @@ declare myCursor cursor for
 		WHERE idCustomer = id
 		ORDER BY tanggalberubah desc
 		LIMIT 1);
-        
+	
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_finished = 1;
 	open myCursor;
+	SET waktu= (SELECT NOW());
     get_id:LOOP
-   
+	
     IF v_finished = 1 
 		THEN LEAVE get_id;
 	END IF;
-    FETCH myCursor INTO namaField1,valueSebelum1;
     SELECT valueSebelum1;
 		IF namaField1 = 'nilaiInvestasi' THEN
+			SET temp = (SELECT Customer.nilaiInvestasi FROM Customer WHERE id = idCustomer);
+            call insertPerubahanCustomer(id,'Customer','nilaiInvestasi',temp,NULL,waktu,idAdmin);
 			UPDATE Customer SET nilaiInvestasi = valueSebelum1 WHERE Customer.idCustomer = id;
 		END IF;
 		IF namaField1 = 'alamat' THEN
+			SET temp = (SELECT Customer.alamat FROM Customer WHERE id = idCustomer);
+			call insertPerubahanCustomer(id,'Customer','alamat',temp,NULL,waktu,idAdmin);
 			UPDATE Customer SET alamat = valueSebelum1 WHERE Customer.idCustomer = id;
 		END IF;
 		IF namaField1 = 'nama' THEN
+			SET temp = (SELECT Customer.nama FROM Customer WHERE id = idCustomer);
+            call insertPerubahanCustomer(id,'Customer','nama',temp,NULL,waktu,idAdmin);
 			UPDATE Customer SET nama = valueSebelum1 WHERE Customer.idCustomer = id;
 		END IF;
 		IF namaField1 = 'idLokasi' THEN
-			UPDATE Customer SET idLokasi = valueSebelum1 WHERE Customer.idCustomer = id;
+			SET temp = (SELECT Customer.idLokasi FROM Customer WHERE id = idCustomer);
+			call insertPerubahanCustomer(id,'Customer','idLokasi',temp,NULL,waktu,idAdmin);
+            UPDATE Customer SET idLokasi = valueSebelum1 WHERE Customer.idCustomer = id;
 		END IF;
 		IF namaField1 = 'tanggalLahir' THEN
+			SET temp = (SELECT Customer.tanggalLahir FROM Customer WHERE id = idCustomer);
+             call insertPerubahanCustomer(id,'Customer','tanggalLahir',temp,NULL,waktu,idAdmin);
 			UPDATE Customer SET tanggalLahir = valueSebelum1 WHERE Customer.idCustomer = id;
 		END IF;
+    FETCH myCursor INTO namaField1,valueSebelum1;
     END LOOP get_id;
     CLOSE myCursor;
     
-    
-   
 END
